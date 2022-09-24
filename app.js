16fab7f497295f0351ad5ef6a821334f773b7d26
rejhaque <rij.rezwan@gmail.com>
2022-09-25 04:04:02 +0600

    working on JS done. App is working properly

--- app.js
+++ app.js
@@ -47,3 +47,44 @@ const info = document.getElementById("info");
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
+
+// ekhon starting e item gula set korbo:
+let currentItem = 0;
+
+// initial item gula load kora
+window.addEventListener("DOMContentLoaded", function(){
+    showPerson();
+});
+
+// show person based on item
+function showPerson(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+nextBtn.addEventListener("click", function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// show prev person
+prevBtn.addEventListener("click", function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// add random access to the reviews array by clicking Surprise me button:
+randomBtn.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
